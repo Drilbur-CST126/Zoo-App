9bcc992e05b93fd13fd7da5b002fdc4098ffa1a5
mapled347 <46737168+mapled347@users.noreply.github.com>
2020-01-25 22:45:31 -0800

    Copy to ZooCompanion folder
    Copy ZooCompanion folder from Branching_off_dana to dana_posey branch.

---
+++ ZooCompanion/ZooCompanion/Controllers/AnimalController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnimalDataAccess;
+
+namespace ZooCompanion.Controllers
+{
+    public class AnimalController : ApiController
+    {
+        public IEnumerable<animals> GetAnimals()
+        {
+            using (ZooCompanionEntities entities = new ZooCompanionEntities())
+            {
+                return entities.animals.ToList();
+            }
+        }
+
+        public animals GetAnimals(string commonName)
+        {
+            using(ZooCompanionEntities entities = new ZooCompanionEntities())
+            {
+                return entities.animals.FirstOrDefault(e => e.common_name == commonName);
+            }
+        }
+    }
+}
