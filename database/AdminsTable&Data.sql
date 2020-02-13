7c7a4fbc06d794c68802e10625d924b636b883d0
mapled347 <46737168+mapled347@users.noreply.github.com>
2020-02-12 22:05:58 -0800

    Added DataSource to AdminPortal
    Corrected admin sql script to make password case sensitve. Added Admin Table to Data Sources for AdminPortal in VS ZooCompanion sln. Need to add references to bind data to controls and write the query cmds.

--- database/AdminsTable&Data.sql
+++ database/AdminsTable&Data.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS admin.admins;
 CREATE TABLE admin.admins (
 	admin_id	INT		PRIMARY KEY,
 	username	VARCHAR(20)	NOT NULL,
-	password	VARCHAR(30) NOT NULL,
+	password	VARCHAR(30) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
 	first_name	VARCHAR(30) NOT NULL,
 	last_name	VARCHAR(30) NOT NULL,
 	email		VARCHAR(50) NOT NULL
