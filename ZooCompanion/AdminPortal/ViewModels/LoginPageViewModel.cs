765f335413e8d6e8ab53e0ad09596ef9387fc997
Drilbur_CST126 <jordan.clark@oit.edu>
2020-02-13 21:57:20 -0800

    Began work on ViewModel for Admin Portal

---
+++ ZooCompanion/AdminPortal/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AdminPortal.ViewModels
+{
+    public class LoginPageViewModel : ViewModelNotifyPropertyChanged, ILoginPageViewModel
+    {
+        
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+        public ICommand LoginCommand { get; }
+    }
+}
