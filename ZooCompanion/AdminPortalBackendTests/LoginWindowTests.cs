dbfa77f205e61390926956e434d8f6342090ecec
Drilbur_CST126 <jordan.clark@oit.edu>
2020-02-17 22:05:47 -0800

    Made Login tests visible

--- ZooCompanion/AdminPortalBackendTests/LoginWindowTests.cs
+++ ZooCompanion/AdminPortalBackendTests/LoginWindowTests.cs
@@ -8,7 +8,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AdminPortalBackendTests
 {
     [TestClass]
-    class LoginWindowTests
+    public class LoginWindowTests
     {
         private LoginPageViewModel viewModel;
 
