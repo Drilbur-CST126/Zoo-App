c5ff3b694aeb97735d9350d84c75d5454e861e0e
mapled347 <46737168+mapled347@users.noreply.github.com>
2020-03-01 15:08:53 -0800

    Added class and exhibit controllers

---
+++ ZooCompanion/ZooCompanion/Controllers/ExhibitController.cs
@@ -0,0 +1,21 @@
+﻿using AnimalDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ZooCompanion.Controllers
+{
+    public class ExhibitController : ApiController
+    {
+        public List<exhibits> Get()
+        {
+            using (ZooCompanionWebApi_dbEntities entities = new ZooCompanionWebApi_dbEntities())
+            {
+                return entities.exhibits.ToList();
+            }
+        }
+    }
+}
