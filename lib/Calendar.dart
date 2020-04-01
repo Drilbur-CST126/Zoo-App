25d2a6a28dfb8578a437bf95f3877b9466e5f475
JayIchimochi <35402378+JayIcihmochi@users.noreply.github.com>
2020-03-31 20:09:04 -0700

    Added fetcher to the Calendar code
    Added the fectcher to the Calendar code. Unfortunately, it doesn't find anything. However, it does not crash when nothing is found, so that is working as intended.

--- lib/Calendar.dart
+++ lib/Calendar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:zoo_app/Calendar/CalendarEventFetcher.dart';
 
+import 'Calendar/event.dart';
 import 'Event.dart';
 
 class CalendarPage extends StatefulWidget{
@@ -10,27 +12,20 @@ class CalendarPage extends StatefulWidget{
 }
 
 class CalendarState extends State<CalendarPage>{
-  Map<DateTime, List> _events;
+  Map<DateTime, List> _events = Map<DateTime,List>();
   List _selectedEvents;
   CalendarController _calendarController = new CalendarController();
+  CalendarEventFetcher fetcher = new CalendarEventFetcher();
 
   @override
   void initState() {
     super.initState();
     final _selectedDay = DateTime.now();
 
-    _events = {
-      _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
-      _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A2', 'Event A7', 'Event B7', 'Event C7'],
-      _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
-      _selectedDay.add(Duration(days: 3)): ['Event A9', 'Event A9', 'Event B9'],
-      _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
-    };
+    List<Event> events = fetcher.getEvents();
+
 
     _selectedEvents = _events.containsKey(_selectedDay) ? _events[_selectedDay] : List();
-
   }
 
   @override
