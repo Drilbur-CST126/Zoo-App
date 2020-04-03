26b06c9a660beafee1e90f58dbf44501fd009abd
mapled347 <46737168+mapled347@users.noreply.github.com>
2020-04-02 19:41:18 -0700

    Story 36 Facts Controller

---
+++ ZooCompanion/ZooCompanion/Controllers/FactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnimalDataAccess;
+
+namespace ZooCompanion.Controllers
+{
+    public class FactController : ApiController
+    {
+        public List<facts> Get()
+        {
+            using (AnimalEntities entities = new AnimalEntities())
+            {
+                return entities.facts.ToList();
+            }
+        }
+    }
+}
---
+++ ZooCompanion/ZooCompanion/Models/Fact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZooCompanion.Models
+{
+    public class Fact
+    {
+        public int FactId { get; set; }
+        public string FactDescription { get; set; }
+        public int AnimalId { get; set; }
+    }
+}
