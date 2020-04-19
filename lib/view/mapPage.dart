51b129e2e0d2899e9784d294b1eeeee237001355
Drilbur_CST126 <jordan.clark@oit.edu>
2020-04-18 22:58:40 -0700

    Fixed map not loading

--- lib/view/mapPage.dart
+++ lib/view/mapPage.dart
@@ -2,11 +2,55 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
+import 'dart:io';
 
-class MapPage extends StatelessWidget
+import 'package:path_provider/path_provider.dart';
+import 'package:zoo_app/view/loadingWidget.dart';
+
+class MapPage extends StatefulWidget
 {
+  static const String _assetDirectory = "assets/ZooMap.PDF";
+
+  @override
+  State<StatefulWidget> createState() {
+    return MapPageState();
+  }
+
+}
+
+class MapPageState extends State<MapPage>
+{
+  String _tempPath = "";
+
+  // Code inspired by https://pspdfkit.com/blog/2019/opening-a-pdf-in-flutter/
+  // Without this page, I wouldn't know to move the ZooMap to a temporary directory
+  // I also wouldn't know how to do it, since Flutter handles assets bizarrely
+  Future<void> openPdfInTempDirectory() async {
+    // PDF cannot be loaded directly from assets, it stops being a File at that point,
+    // so we need to make a temp file (as far as I know)
+    final bytes = await DefaultAssetBundle.of(context).load(MapPage._assetDirectory);
+    final list = bytes.buffer.asUint8List();
+
+    final tempDir = await getTemporaryDirectory();
+    _tempPath = "${tempDir.path}/${MapPage._assetDirectory}";
+    final file = await File(_tempPath).create(recursive: true);
+    file.writeAsBytesSync(list);
+  }
+
+  Widget _createPdfViewer(BuildContext context, AsyncSnapshot<void> snapshot)
+  {
+    if (_tempPath == "")
+    {
+      return LoadingWidget();
+    }
+    else
+    {
+      return PDFViewerScaffold(path: _tempPath,);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PDFViewerScaffold(path: "assets/ZooMap.PDF",);
+    return FutureBuilder(builder: _createPdfViewer, future: openPdfInTempDirectory(),);
   }
 }
