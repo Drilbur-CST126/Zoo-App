d73b3c255c01674e264b92e17648a6ae9ac4da49
mapled347 <46737168+mapled347@users.noreply.github.com>
2020-04-29 23:37:07 -0700

    Story 41: Add Controller for Images

--- database/PictureData.sql
+++ database/PictureData.sql
@@ -2,9 +2,9 @@
 
 --INSERT INTO animal.pictures (file_name, picture_url, animal_id)
 --VALUES ('AfricanBullFrog1.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/EVnZOhAqCzxKupx5cL2eW3kB_qZBfRMtAAkxb-Xko4K0JQ?e=k0z2xE', 1);
-INSERT INTO animal.pictures (file_name, picture_url, animal_id)
-VALUES ('AfricanCrestedPorcupine1.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/EYI2B4N6CTpLnMte48Yz7KMBD8MAlkC_VBloGl2PiUVPpA?e=iYCOjS', 2);
-INSERT INTO animal.pictures (file_name, picture_url, animal_id)
-VALUES ('AfricanCrestedPorcupine2.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/ESmqvYeqiA9JltyeIYJcwcsBB8ElT5kNXeOlAfAnZFXB6g?e=jVMoqT', 2);
-INSERT INTO animal.pictures (file_name, picture_url, animal_id)
-VALUES ('AfricanCrestedPorcupine3.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/Ee8t1hAyy_JHkuneLZBc0noBVy91wCgB9qaCo6dFNWeilA?e=tEBS0z', 2);
+--INSERT INTO animal.pictures (file_name, picture_url, animal_id)
+--VALUES ('AfricanCrestedPorcupine1.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/EYI2B4N6CTpLnMte48Yz7KMBD8MAlkC_VBloGl2PiUVPpA?e=iYCOjS', 2);
+--INSERT INTO animal.pictures (file_name, picture_url, animal_id)
+--VALUES ('AfricanCrestedPorcupine2.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/ESmqvYeqiA9JltyeIYJcwcsBB8ElT5kNXeOlAfAnZFXB6g?e=jVMoqT', 2);
+--INSERT INTO animal.pictures (file_name, picture_url, animal_id)
+--VALUES ('AfricanCrestedPorcupine3.jpg', 'https://oregontech-my.sharepoint.com/:i:/g/personal/dana_posey_oit_edu/Ee8t1hAyy_JHkuneLZBc0noBVy91wCgB9qaCo6dFNWeilA?e=tEBS0z', 2);
